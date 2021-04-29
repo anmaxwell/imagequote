c6e96596f0c851d5f91d92406523f43c9e9bb7a6
anmaxwell <aniamaxwell@gmail.com>
2021-04-29 21:07:38 +0100

    added image selection model and updated page display

--- imageclassification/imagemodel.py
+++ imageclassification/imagemodel.py
@@ -1,9 +1,15 @@
 import tensorflow as tf
 import tensorflow_hub as hub
+import csv
+import string
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-image_select():
+def image_select():
+
+    df = pd.read_csv('imageclassification/imagequotes.csv')
+    text = 'As human beings our greatness lies not so much in being able to remake the world - that is the myth of the atomic age - as in being able to remake ourselves.'
 
     module_url = 'https://tfhub.dev/google/universal-sentence-encoder/4'
     model = hub.load(module_url)
@@ -12,7 +18,7 @@ image_select():
 
     dataset = pd.DataFrame(df['Vector'].tolist(), index=df.index)
 
-    X = dataset, axis=1)
+    X = dataset
     y = df['Image']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
@@ -20,4 +26,7 @@ image_select():
     knn_classifier = KNeighborsClassifier()
     knn_classifier.fit(X_train, y_train)
 
-    knn_predictions = knn_classifier.predict('quote')
+    quote = model([text])
+    knn_predictions = knn_classifier.predict(quote)
+
+    return knn_predictions, text
--- imageclassification/routes.py
+++ imageclassification/routes.py
@@ -1,11 +1,15 @@
 from imageclassification import app
+from imageclassification.imagemodel import image_select
 from flask import render_template
 from random import randrange
 
 @app.route('/') #Page to display quote and image
 def index():
 
-    image = randrange(1,22) 
-    background = "%s%s" % (image, ".jpg")
+    prediction, text = image_select()
+    background = "%s%s" % (prediction[0], '.jpg')
 
-    return render_template('index.html', background=background)
+    #text = 'As human beings our greatness lies not so much in being able to remake the world - that is the myth of the atomic age - as in being able to remake ourselves.'
+    #background = '1.jpg'
+
+    return render_template('index.html', background=background, text=text)
